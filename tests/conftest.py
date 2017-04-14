8a4f04142aec3798684ef4410b53e6916becd8ef
Michael van Tellingen <michael@mvantellingen.nl>
2017-04-14 14:37:05 +0200

    Add fixture for config yml

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,15 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 @pytest.fixture
 def definition():
-    path = os.path.join(BASE_DIR, 'task_definitions/default.json')
+    path = os.path.join(BASE_DIR, 'files/default_taskdef.json')
 
     with open(path, 'rb') as json_file:
         return json.load(json_file)
+
+
+@pytest.fixture
+def default_config():
+    path = os.path.join(BASE_DIR, 'files/default_config.yml')
+
+    with open(path, 'rb') as fh:
+        yield fh
