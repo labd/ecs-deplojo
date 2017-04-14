7131fe0d8fa18979886313b758982e6b4e283574
Michael van Tellingen <michael@mvantellingen.nl>
2017-04-14 15:10:54 +0200

    Add unittest for generate_task_definitions

--- src/ecs_deplojo/task_definitions.py
+++ src/ecs_deplojo/task_definitions.py
@@ -5,8 +5,17 @@ from string import Template
 
 def generate_task_definitions(config, template_vars, base_path,
                               output_path=None):
-    """Generate the task definitions"""
+    """Generate the task definitions
+
+    :parameter config: The yaml config contents
+    :parameter template_vars: Key-Value dict with template replacements
+    :parameter base_path: The base path (location of the config file)
+    :parameter output_path: Optional path to write the task definitions to.
+    :rtype dict:
+
+    """
     task_definitions = {}
+
     for name, info in config['task_definitions'].items():
         # Default environment. Always create a new dict instance since it is
         # mutated.
@@ -19,13 +28,11 @@ def generate_task_definitions(config, template_vars, base_path,
             env_items.update(config['environment_groups'][env_group])
 
         overrides = info.get('overrides', {})
-        container_overrides = overrides.get(name, {})
-
         definition = generate_task_definition(
             info['template'],
             env_items,
             template_vars,
-            container_overrides,
+            overrides,
             name=name,
             base_path=base_path)
 
@@ -44,6 +51,8 @@ def generate_task_definition(filename, environment, template_vars, overrides,
     if base_path:
         filename = os.path.join(base_path, filename)
 
+    print(overrides)
+
     with open(filename, 'rb') as fh:
         data = json.load(fh)
 
@@ -53,7 +62,8 @@ def generate_task_definition(filename, environment, template_vars, overrides,
             container['environment'] = environment
 
             if overrides:
-                for key, value in overrides.items():
+                container_overrides = overrides.get(container['name'], {})
+                for key, value in container_overrides.items():
                     if key in container and isinstance(container[key], list):
                         container[key].extend(value)
                     elif key in container and isinstance(container[key], dict):
--- tests/test_task_definitions.py
+++ tests/test_task_definitions.py
@@ -7,26 +7,26 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def test_generate_task_definition(tmpdir):
     task_data = """
-{
-  "family": "default",
-  "volumes": [],
-  "containerDefinitions": [
     {
-      "name": "default",
-      "image": "${image}",
-      "essential": true,
-      "command": ["hello", "world"],
-      "memory": 256,
-      "cpu": 0,
-      "portMappings": [
+      "family": "default",
+      "volumes": [],
+      "containerDefinitions": [
         {
-          "containerPort": 8080,
-          "hostPort": 0
+          "name": "default",
+          "image": "${image}",
+          "essential": true,
+          "command": ["hello", "world"],
+          "memory": 256,
+          "cpu": 0,
+          "portMappings": [
+            {
+              "containerPort": 8080,
+              "hostPort": 0
+            }
+          ]
         }
       ]
     }
-  ]
-}
     """.strip()
 
     filename = tmpdir.join('task_definition.json')
@@ -67,26 +67,26 @@ def test_generate_task_definition(tmpdir):
 
 def test_generate_task_definition_overrides(tmpdir):
     task_data = """
-{
-  "family": "default",
-  "volumes": [],
-  "containerDefinitions": [
     {
-      "name": "default",
-      "image": "${image}",
-      "essential": true,
-      "command": ["hello", "world"],
-      "memory": 256,
-      "cpu": 0,
-      "portMappings": [
+      "family": "default",
+      "volumes": [],
+      "containerDefinitions": [
         {
-          "containerPort": 8080,
-          "hostPort": 0
+          "name": "default",
+          "image": "${image}",
+          "essential": true,
+          "command": ["hello", "world"],
+          "memory": 256,
+          "cpu": 0,
+          "portMappings": [
+            {
+              "containerPort": 8080,
+              "hostPort": 0
+            }
+          ]
         }
       ]
     }
-  ]
-}
     """.strip()
 
     filename = tmpdir.join('task_definition.json')
@@ -99,14 +99,16 @@ def test_generate_task_definition_overrides(tmpdir):
             'image': 'my-docker-image:1.0',
         },
         overrides={
-            'memory': 512,
-            'memoryReservation': 128,
-            'portMappings': [
-                {
-                    'hostPort': 80,
-                    'containerPort': 9000,
-                }
-            ],
+            'default': {
+                'memory': 512,
+                'memoryReservation': 128,
+                'portMappings': [
+                    {
+                        'hostPort': 80,
+                        'containerPort': 9000,
+                    }
+                ],
+            }
         },
         name='my-task-def',
     )
@@ -135,5 +137,180 @@ def test_generate_task_definition_overrides(tmpdir):
                 'environment': {}
             }
         ]
+    }
+    assert result == expected
+
+
+def test_generate_task_definitions(tmpdir):
+    task_data = """
+    {
+      "family": "default",
+      "volumes": [],
+      "containerDefinitions": [
+        {
+          "name": "web-1",
+          "image": "${image}",
+          "essential": true,
+          "command": ["hello", "world"],
+          "memory": 256,
+          "cpu": 0,
+          "portMappings": [
+            {
+              "containerPort": 8080,
+              "hostPort": 0
+            }
+          ]
+        },
+        {
+          "name": "web-2",
+          "image": "${image}",
+          "essential": true,
+          "command": ["hello", "world"],
+          "memory": 256,
+          "cpu": 0,
+          "portMappings": [
+            {
+              "containerPort": 8080,
+              "hostPort": 0
+            }
+          ]
+        }
+      ]
+    }
+    """.strip()
+
+    filename = tmpdir.join('task_definition.json')
+    filename.write(task_data)
+
+    config = {
+        'environment': {
+            'DATABASE_URL': 'postgresql://',
+        },
+        'environment_groups': {
+            'group-1': {
+                'ENV_CODE': 'group-1',
+            },
+            'group-2': {
+                'ENV_CODE': 'group-2',
+            },
+        },
+        'task_definitions': {
+            'task-def-1': {
+                'template': filename.strpath,
+                'environment_group': 'group-1',
+                'overrides': {
+                    'web-1': {
+                        'memory': 512,
+                    }
+                }
+            },
+            'task-def-2': {
+                'template': filename.strpath,
+                'environment_group': 'group-2',
+                'overrides': {
+                    'web-1': {
+                        'memory': 512,
+                    }
+                }
+            }
+        }
+    }
+
+    result = task_definitions.generate_task_definitions(
+        config,
+        template_vars={
+            'image': 'my-docker-image:1.0',
+        },
+        base_path=None)
+
+    expected = {
+        'task-def-1': {
+            'definition': {
+                'family': 'task-def-1',
+                'volumes': [],
+                'containerDefinitions': [
+                    {
+                        'name': 'web-1',
+                        'image': 'my-docker-image:1.0',
+                        'essential': True,
+                        'command': ['hello', 'world'],
+                        'memory': 512,
+                        'cpu': 0,
+                        'portMappings': [
+                            {
+                                'containerPort': 8080,
+                                'hostPort': 0
+                            }
+                        ],
+                        'environment': {
+                            'DATABASE_URL': 'postgresql://',
+                            'ENV_CODE': 'group-1',
+                        }
+                    },
+                    {
+                        'name': 'web-2',
+                        'image': 'my-docker-image:1.0',
+                        'essential': True,
+                        'command': ['hello', 'world'],
+                        'memory': 256,
+                        'cpu': 0,
+                        'portMappings': [
+                            {
+                                'containerPort': 8080,
+                                'hostPort': 0
+                            }
+                        ],
+                        'environment': {
+                            'DATABASE_URL': 'postgresql://',
+                            'ENV_CODE': 'group-1',
+                        }
+                    }
+                ]
+            },
+        },
+        'task-def-2': {
+            'definition': {
+                'family': 'task-def-2',
+                'volumes': [],
+                'containerDefinitions': [
+                    {
+                        'name': 'web-1',
+                        'image': 'my-docker-image:1.0',
+                        'essential': True,
+                        'command': ['hello', 'world'],
+                        'memory': 512,
+                        'cpu': 0,
+                        'portMappings': [
+                            {
+                                'containerPort': 8080,
+                                'hostPort': 0
+                            }
+                        ],
+                        'environment': {
+                            'DATABASE_URL': 'postgresql://',
+                            'ENV_CODE': 'group-2',
+                        }
+                    },
+                    {
+                        'name': 'web-2',
+                        'image': 'my-docker-image:1.0',
+                        'essential': True,
+                        'command': ['hello', 'world'],
+                        'memory': 256,
+                        'cpu': 0,
+                        'portMappings': [
+                            {
+                                'containerPort': 8080,
+                                'hostPort': 0
+                            }
+                        ],
+                        'environment': {
+                            'DATABASE_URL': 'postgresql://',
+                            'ENV_CODE': 'group-2',
+                        }
+                    }
+                ]
+            }
+        }
     }
     assert result == expected
