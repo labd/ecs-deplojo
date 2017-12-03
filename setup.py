32815ee991bd035cdfcdb0e9a3a2a4344a52c43d
Michael van Tellingen <michael@mvantellingen.nl>
2017-12-03 19:31:33 +0100

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open('README.rst') as fh:
 
 setup(
     name='ecs-deplojo',
-    version='0.4.0',
+    version='0.5.0',
     author='Lab Digital B.V.',
     author_email='opensource@labdigital.nl',
     url='https://www.github.com/labd/ecs-deplojo/',
